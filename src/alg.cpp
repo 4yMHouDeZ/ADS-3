8405b0240f5717da263210972a3d290126510a63
4yMHouDeZ <sergoart1234@gmail.com>
2026-03-12 11:13:20 +0300

    att 1

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,15 +3,47 @@
 #include "alg.h"
 
 uint64_t collatzMaxValue(uint64_t num) {
+  uint64_t max = 0;
+  while (num > 1) {
+    num = nextElement(num);
+    if (max < num) {
+      max = num;
+    }
+  }
   return 1;
 }
 
 unsigned int collatzLen(uint64_t num) {
-  return 1;
+  unsigned int len = 1;
+  while (num > 1) {
+    num = nextElement(num);
+    len++;
+  }
+  return len;
 }
 
 unsigned int seqCollatz(unsigned int *maxlen,
                         uint64_t lbound,
                         uint64_t rbound) {
-  return 1;
+  *maxlen = 0;
+  uint64_t num, n;
+  for (uint64_t i = 2; i < 1000000; i++) {
+    n = i;
+    uint64_t counter = 1;
+    while (n > 1) {
+      n = nextElement(n);
+      counter++;
+    }
+    if (*maxlen < counter & counter <= rbound & counter >= lbound) {
+      num = n;
+      *maxlen = counter;
+    }
+  }
+  return num;
+}
+
+uint64_t nextElement(uint64_t n) {
+  if (n % 2 == 0) n = n / 2;
+  else n = 3 * n + 1;
+  return n;
 }
