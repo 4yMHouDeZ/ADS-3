8ce1604c052fcb79c39117c8f31fe0f9b989f085
4yMHouDeZ <sergoart1234@gmail.com>
2026-03-15 16:10:57 +0300

    att 4

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,13 +5,13 @@
 uint64_t collatzMaxValue(uint64_t num) {
   uint64_t max = 0;
   while (num > 1) {
-    if (n % 2 == 0) n = n / 2;
-    else n = 3 * n + 1;
+    if (num % 2 == 0) num /= 2;
+    else num = 3 * num + 1;
     if (max < num) {
       max = num;
     }
   }
-  return 1;
+  return max;
 }
 
 unsigned int collatzLen(uint64_t num) {
